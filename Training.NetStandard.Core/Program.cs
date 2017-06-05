715b852300377bec11aeeee9e1c8f2fb9a6a3436
Łukasz Klimek <lklimek@future-processing.com>
2017-06-05 09:09:27 +0200

    added sample solution

---
+++ Training.NetStandard.Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Training.NetStandard.Lib;
+
+namespace Training.NetStandard.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeter = new Common();
+            greeter.WriteGreeting(".NET Core");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Training.NetStandard.Lib/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Training.NetStandard.Lib
+{
+    public class Common
+    {
+        public string Greet(string name) =>
+            $"Hello, {name}!";
+
+        public void WriteGreeting(string name) =>
+            Console.WriteLine(Greet(name));
+    }
+}
